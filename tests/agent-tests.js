3ef8eae048e767fc772576d222ca05e72c59d6a7
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-07 15:22:47 -0600

    implementacion de npm i --save defaults

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -2,6 +2,18 @@
 
 const test = require('ava')
 
-test('make it pass', t => {
-    t.pass()
+let config = {
+    logging:function () {}
+}
+
+let db = null
+
+test.beforeEach(async () => {
+    const setupDatabase = require('../')
+    db = await setupDatabase(config)
+}) 
+
+test('Agent', t => { // verificacion de que Agent exista
+    t.truthy(db.Agent, 'Agent service should exist') // si se resulve el valor a verdadero
+
 })
