46753753399fa17b5ea3936ec59d50e5ba3b6ee7
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-07 12:41:41 -0600

    modificacion de setup1

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 'use strict'
 
-module.export = async function (config) {
+
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
+module.exports = async function (config) {
+
+    const sequelize = setupDatabase(config)
+    const AgentModel = setupAgentModel(config)
+    const MetricModel = setupMetricModel(config)
+// definimos relacion agtente y metricas
+    AgentModel.hasMany(MetricModel)
+    MetricModel.belongsTo(AgentModel)
+// espera hasta que si se conecte adecuadamente
+   await sequelize.authenticate()
+// configuracion de la base de datos
+   if (config.setup) {
+       await sequelize.sync({ force: true })// si la db existe borrara la db nueva
+   }
+
+
+
+
+
+
     const Agent ={}
     const Metric = {}
+
     return {
         Agent,
         Metric
---
+++ iot-db/setup.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const debug = require('debug')('iot_nodejs:db:setup')
+const db = require('../lib/db')
+
+async function setup () {
+  const config = {
+    database: process.env.DB_NAME || 'iot_nodejs',
+    username: process.env.DB_USER || 'IOT_NODEJS',
+    password: process.env.DB_PASS || 'IOT_NODEJS',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true,
+    operatorsAliases: false
+
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
