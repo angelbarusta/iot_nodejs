2e5861b4c70bc1ee9ffd693c639a3a6546838225
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-07 18:06:00 -0600

    test con cinon correcto, ok

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -5,11 +5,11 @@ const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
 let config = {
-  logging: function () {}
+    logging:function () {}
 }
 
 let MetricStub = {
-  belongsTo: sinon.spy()// un spy es un funcion que nos dice datos sobre la funcion, es un chicmoso vamos
+    belongsTo: sinon.spy()// un spy es un funcion que nos dice datos sobre la funcion, es un chicmoso vamos 
 }
 
 let AgentStub = null
@@ -17,30 +17,34 @@ let db = null
 let sandbox = null
 
 test.beforeEach(async () => {
-  sandbox = sinon.createSandbox
+    sandbox = sinon.createSandbox()
 
-  AgentStub = {
-    // un sandbox es un ambiente especifico que solo funciona para este caso en particular
-    hasMany: sandbox.spy()
-  }
+    AgentStub = {
+        //un sandbox es un ambiente especifico que solo funciona para este caso en particular
+        hasMany: sandbox.spy()
+    }
 
-  const setupDatabase = proxyquire('../', {
-    './models/agent': () => AgentStub, // las mismas rutas que las que estan e declaradas en index.js
-    './models/metric': () => MetricStub
-  })
 
-  db = await setupDatabase(config)
-})
+    const setupDatabase = proxyquire('../', {
+        './models/agent': () => AgentStub,  // las mismas rutas que las que estan e declaradas en index.js
+        './models/metric': () => MetricStub 
+    })
 
-test.afterEach(() => {
-  sandbox && sinon.sandbox.restore()
+
+    db = await setupDatabase(config)
+}) 
+
+test.afterEach(t => {
+    sandbox && sandbox.restore()
 })
 
 test('Agent', t => { // verificacion de que Agent exista
-  t.truthy(db.Agent, 'Agent service should exist') // si se resulve el valor a verdadero
+    t.truthy(db.Agent, 'Agent service should exist') // si se resulve el valor a verdadero
+
 })
 test.serial('Setup', t => {
-  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany fue ejecutada')
-  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argumento de esa funcion')
-  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo fue ejecutada')
+    t.true(AgentStub.hasMany.called,'AgentModel.hasMany fue ejecutada')
+    t.true(AgentStub.hasMany.calledWith(MetricStub),'Argumento de esa funcion')
+    t.true(MetricStub.belongsTo.called,'MetricModel.belongsTo fue ejecutada')
+    t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argumentos de esa funcion')
 })
