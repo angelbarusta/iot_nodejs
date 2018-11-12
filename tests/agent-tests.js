449bb73580dfa501d0d58669cdac84a4ae8a682d
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-12 16:25:17 -0600

    implementacion de test servicio agent

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -3,48 +3,54 @@
 const test = require('ava')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
+const agentFixtures = require('./fixtures/agent')
 
 let config = {
-    logging:function () {}
+  logging: function () {}
 }
 
 let MetricStub = {
-    belongsTo: sinon.spy()// un spy es un funcion que nos dice datos sobre la funcion, es un chicmoso vamos 
+  belongsTo: sinon.spy()// un spy es un funcion que nos dice datos sobre la funcion, es un chicmoso vamos
 }
 
+let single = Object.assign({}, agentFixtures.single)
+let id = 1
 let AgentStub = null
 let db = null
 let sandbox = null
 
 test.beforeEach(async () => {
-    sandbox = sinon.createSandbox()
+  sandbox = sinon.createSandbox()
 
-    AgentStub = {
-        //un sandbox es un ambiente especifico que solo funciona para este caso en particular
-        hasMany: sandbox.spy()
-    }
+  AgentStub = {
+    // un sandbox es un ambiente especifico que solo funciona para este caso en particular
+    hasMany: sandbox.spy()
+  }
 
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub, // las mismas rutas que las que estan e declaradas en index.js
+    './models/metric': () => MetricStub
+  })
 
-    const setupDatabase = proxyquire('../', {
-        './models/agent': () => AgentStub,  // las mismas rutas que las que estan e declaradas en index.js
-        './models/metric': () => MetricStub 
-    })
-
-
-    db = await setupDatabase(config)
-}) 
+  db = await setupDatabase(config)
+})
 
 test.afterEach(t => {
-    sandbox && sandbox.restore()
+  sandbox && sandbox.restore()
 })
 
 test('Agent', t => { // verificacion de que Agent exista
-    t.truthy(db.Agent, 'Agent service should exist') // si se resulve el valor a verdadero
-
+  t.truthy(db.Agent, 'Agent service should exist') // si se resulve el valor a verdadero
 })
 test.serial('Setup', t => {
-    t.true(AgentStub.hasMany.called,'AgentModel.hasMany fue ejecutada')
-    t.true(AgentStub.hasMany.calledWith(MetricStub),'Argumento de esa funcion')
-    t.true(MetricStub.belongsTo.called,'MetricModel.belongsTo fue ejecutada')
-    t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argumentos de esa funcion')
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany fue ejecutada')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argumento de esa funcion')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo fue ejecutada')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argumentos de esa funcion')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'deberia ser el mismo')
 })
--- tests/fixtures/agent.js
+++ tests/fixtures/agent.js
@@ -1,36 +1,39 @@
 'use strict'
 
 const agent = {
-    id: 1,
-    uuid: 'yyy-yyy-yyy',
-    name: 'fixture',
-    username: 'iot',
-    hostname: 'test-host',
-    pid: 0,
-    connected: true,
-    createdAt: new Date(),
-    updateAt: new Date()
+  id: 1,
+  uuid: 'yyy-yyy-yyy',
+  name: 'fixture',
+  username: 'iot',
+  hostname: 'test-host',
+  pid: 0,
+  connected: true,
+  createdAt: new Date(), // fecha de craciom
+  updatedAt: new Date()// fecha de actualizacion
 }
 
 const agents = [
-    agent,
-    extend(agent, { id: 2, uuid: 'yyy-yyy-yyw', connected: false, username: 'test' }),
-    extend(agent, { id: 3, uuid: 'yyy-yyy-yyx,' }),
-    extend(agent, { id: 4, uuid: 'yyy-yyy-yyz', username: 'test' })
+  agent,
+  extend(agent, { id: 2, uuid: 'yyy-yyy-yyw', connected: false, username: 'test' }),
+  extend(agent, { id: 3, uuid: 'yyy-yyy-yyx,' }),
+  extend(agent, { id: 4, uuid: 'yyy-yyy-yyz', username: 'test' })
 ]
 
-function extended (obj,values) {
-    const clone = Object.assign({}, obj)
-    return Object.assign(clone, values)
+function extend (obj, values) { // funcion para sobre escribir un objeto
+  const clone = Object.assign({}, obj)
+  return Object.assign(clone, values)
 }
 
 module.exports = {
-    single: agent,
-    all: agents,
-    connected: agents.filter(a => a.connected),
-    iot: agents.filter(a => a.usarname === 'iot'),// para filtrar por usuario
-    byUuid: id => agents.filter(a => a.uuid === id).shift(),// para filtrar por uuid
-    byId: id => agents.filter(a => a.id === id).shift()// para filtrar por id
-
+  single: agent, // filtros
+  all: agents, // filtros
+  connected: agents.filter(a => a.connected),
+  iot: agents.filter(a => a.username === 'iot'), // para filtrar por usuario
+  byUuid: id => agents.filter(a => a.uuid === id).shift(), // para filtrar por uuid
+  byId: id => agents.filter(a => a.id === id).shift()// para filtrar por id
 
 }
+// primero abrir en terminal: node
+// despues >var agentFixtures = require('./tests/fixtures/agent')
+// despues >agentFixtures.single;
+// > agentFixtures.byId(1)
