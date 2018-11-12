27baba6c4b7cc047701d9ce03605a7063cbd7b25
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-12 17:26:58 -0600

    implementacion de createOrUpdate

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -15,9 +15,16 @@ let MetricStub = {
 
 let single = Object.assign({}, agentFixtures.single)
 let id = 1
+let uuid = 'yyy-yyy-yyy'
 let AgentStub = null
 let db = null
 let sandbox = null
+
+let uuidArgs = {
+  where: {
+    uuid
+  }
+}
 
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
@@ -27,9 +34,18 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   }
 
+  //Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
+
   //Modelo findById Stub
   AgentStub.findById = sandbox.stub()
   AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+  //Model update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub, // las mismas rutas que las que estan e declaradas en index.js
@@ -61,4 +77,10 @@ test.serial('Agent#findById', async t => {
   t.true(AgentStub.findById.calledWith(id), 'findById debera llamar con los argumentos que son')
 
   t.deepEqual(agent, agentFixtures.byId(id), 'deberia ser el mismo')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.deepEqual(agent,  single, 'el agent deberia ser el mismo')
 })
